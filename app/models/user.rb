bbce86956e7bcdf6781d65d62516368a69ae0715
tundeiness <tundeiness@yahoo.co.uk>
2022-09-17 19:03:22 +0100

    wip bug fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         :jwt_authenticable, jwt_revocation_strategy: self
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   def jwt_payload
     super
