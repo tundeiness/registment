b3d68caa83e6ce0a6c74c0de183d53eab67bd2a5
tundeiness <tundeiness@yahoo.co.uk>
2022-09-17 11:29:46 +0100

    edit session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  respond_to :json 
+
+
+  private 
+
+  def respond_with(resource, options={}) 
+      render json: {
+        status: { code: 200, message: 'Sign in successful', data: current_user }
+      }, status: :ok
+    
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split('')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: 200,
+        message: 'sign out successful'
+      }, status: :ok
+    else 
+      render json: {
+        status: 401,
+        message: "user hsa no active session"
+      }, status: :unauthorized 
+    end
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
