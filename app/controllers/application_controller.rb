8fd437e94eb800a2d91070a19518eb0a9954575c
tundeiness <tundeiness@yahoo.co.uk>
2022-09-18 12:35:16 +0100

    add new fields for account update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |param| param.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :current_password) }
   end
 end
