7e4b8cadaa42fb333f172cd678eee996a99c3229
tundeiness <tundeiness@yahoo.co.uk>
2022-09-18 13:26:08 +0100

    add namespace routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do 
+    namespace :v1 do 
+      #  user resources
+    end
+  end
 end
