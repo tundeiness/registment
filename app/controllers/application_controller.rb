fccbc32f98223a8e88f1501708eff678909cd65b
tundeiness <tundeiness@yahoo.co.uk>
2022-09-18 12:23:50 +0100

    add permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_filter :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |param| param.permit(:name, :email, :password, :current_password) }
+  end
 end
