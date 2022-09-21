7481f4172742ccbff11fad0e0fd08dbd2556c11e
tundeiness <tundeiness@yahoo.co.uk>
2022-09-21 14:44:03 +0100

    add validations to equipment model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,5 @@
 class Equipment < ApplicationRecord
   # belongs_to :user
   has_one_attached :photo
+  validates :brand_name, :serial_no, :condition, :date_acquired, :supplier, :model_number, :description, presence: true
 end
