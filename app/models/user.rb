bcc9ab5d4d9b9c34df198fc1668eb71df3099b4e
tundeiness <tundeiness@yahoo.co.uk>
2022-09-21 13:33:56 +0100

    migrate equipment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  has_many :equipments
+
   def jwt_payload
     super
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_183832) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_122441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "equipment", force: :cascade do |t|
+    t.string "brand_name", null: false
+    t.string "serial_no", null: false
+    t.string "condition", null: false
+    t.date "date_acquired", null: false
+    t.string "supplier", null: false
+    t.string "model_number", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_equipment_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_183832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "equipment", "users"
 end
