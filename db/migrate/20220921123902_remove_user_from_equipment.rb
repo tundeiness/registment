f6ecbfa240352c1df4570490dda2a323ee9bd7be
tundeiness <tundeiness@yahoo.co.uk>
2022-09-21 13:40:29 +0100

    remove user id from equipment model

---
+++ db/migrate/20220921123902_remove_user_from_equipment.rb
@@ -0,0 +1,5 @@
+class RemoveUserFromEquipment < ActiveRecord::Migration[7.0]
+  def change
+    remove_reference :equipment, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_122441) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_123902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,10 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_122441) do
     t.string "supplier", null: false
     t.string "model_number", null: false
     t.text "description", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_equipment_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +44,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_122441) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "equipment", "users"
 end
