d494adf769ed9dab90746c9d39921908384fe30a
tundeiness <tundeiness@yahoo.co.uk>
2022-09-22 12:37:11 +0100

    implement index action for equipment controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::EquipmentsController < ApplicationController
+
+  def index
+    @equipments = Equipment.all
+    render json: @equipments.map { |item|
+      if item.photo.attached?
+        item.as_json(only: :brand_name).merge(photo_path: url_for(item.photo))
+      else
+        item.as_json only: :brand_name
+      end
+    }
+  end
+
+  private 
+
+  def equipment_params
+    params.require(:equipment).permit(:brand_name, :photo, :serial_no, :condition, :date_acquired, :supplied_by, :model_number, :description)
+  end
 end
