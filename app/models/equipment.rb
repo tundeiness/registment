020abc7635861e9addc455b253bbf2a2a3b11159
tundeiness <tundeiness@yahoo.co.uk>
2022-09-22 12:18:22 +0100

    edit equipment model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,8 @@
 class Equipment < ApplicationRecord
   # belongs_to :user
-  has_one_attached :photo
+  has_one_attached :photo do |photo|
+    photo.variant :thumbnail, resize_to_limit: [100, 100]
+    photo.variant :medium, resize_to_limit: [400, 400]
+  end
   validates :brand_name, :serial_no, :condition, :date_acquired, :supplier, :model_number, :description, presence: true
 end
