020711d884afd083d3a83357b829880bfbf8dccb
tundeiness <tundeiness@yahoo.co.uk>
2022-09-22 13:04:18 +0100

    add show action in equipments controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -11,6 +11,15 @@ class Api::V1::EquipmentsController < ApplicationController
     }
   end
 
+  def show
+    @equipments = Equipment.find(params[:id])
+    if @equipments.photo.attached? 
+      render json: @equipments.as_json(only: %i[brand_name]).merge(photo_path: url_for(@equipments.avatar))
+    else 
+      render json: @equipments.as_json only: :brand_name # rubocop:disable Lint/Syntax
+    end 
+  end 
+
   private 
 
   def equipment_params
