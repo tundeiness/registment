744bfb0c0f1ce666fb998ef8a1ce7cef70e770f2
tundeiness <tundeiness@yahoo.co.uk>
2022-09-23 16:13:37 +0100

    setup rspec test for equipment

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :equipment do
-    brand_name { "MyString" }
-    serial_no { "MyString" }
-    condition { "MyString" }
-    date_acquired { "2022-09-21" }
-    supplier { "MyString" }
-    model_number { "MyString" }
-    description { "MyText" }
-    user { nil }
+    brand_name { 'MyString' }
+    serial_no { 'MyString' }
+    condition { 'MyString' }
+    date_acquired { '2022-09-21' }
+    supplier { 'MyString' }
+    model_number { 'MyString' }
+    description { 'MyText' }
+    # user { nil }
   end
 end
+# equip = FactoryBot.create(:equipment)
+# equip.brand_name == 'MyString'
--- spec/models/equipment_spec.rb
+++ spec/models/equipment_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Equipment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a brand_name' do
+    equip = FactoryBot.build(:equipment, brand_name: '')
+    expect(equip.brand_name).to be_empty
+  end
 end
