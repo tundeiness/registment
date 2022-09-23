9fd6003e1ea80e1bbbfa2dd12af004c47000b302
tundeiness <tundeiness@yahoo.co.uk>
2022-09-23 16:18:32 +0100

    edit equipment spec

--- spec/models/equipment_spec.rb
+++ spec/models/equipment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Equipment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   it 'must have a brand_name' do
-    equip = FactoryBot.build(:equipment, brand_name: '')
+    equip = build(:equipment, brand_name: '')
     expect(equip.brand_name).to be_empty
   end
 end
