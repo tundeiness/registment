66920a9b7004e528bb85a80398d97fb2a5d5e6eb
tundeiness <tundeiness@yahoo.co.uk>
2022-09-26 18:01:10 +0100

    add trait for condition

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,16 +1,20 @@
 FactoryBot.define do
   factory :equipment do
     brand_name { Faker::Company.name }
-    serial_no { Faker::Company.duns_number }
-    condition { 'MyString' }
-    date_acquired { '2022-09-21' }
-    supplier { 'MyString' }
-    model_number { 'MyString' }
-    description { 'MyText' }
-    name { Faker::Restaurant.name }
+    serial_no { Faker::Alphanumeric.alphanumeric(number: 15) }
+    # condition { 'MyString' }
+    trait :broken do
+      condition :broken
+    end
+
+    trait :intact do
+      condition :intact
+    end
+
+    date_acquired { Faker::Date.in_date_period }
+    supplier { Faker::Name.name }
+    model_number { Faker::Company.duns_number }
     description { Faker::Lorem.paragraph }
-    ingredients { Faker::Lorem.sentence }
-    image { Faker::Avatar.image }
   end
 end
 # equip = FactoryBot.create(:equipment)
