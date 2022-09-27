fe04c7a8791b645dda7ba97465f414bbb9b114a5
tundeiness <tundeiness@yahoo.co.uk>
2022-09-27 15:39:04 +0100

    remove excess code block

--- spec/models/equipment_spec.rb
+++ spec/models/equipment_spec.rb
@@ -2,83 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Equipment, type: :model do
  
-  describe 'brand_name' do
-    it 'must have a brand_name' do
-      equip = build(:equipment, brand_name: '')
-      expect(equip.brand_name).to be_empty
-    end
+  # describe 'brand_name' do
+  #   it 'must have a brand_name' do
+  #     equip = build(:equipment, brand_name: '')
+  #     expect(equip.brand_name).to be_empty
+  #   end
 
-    it 'has a brand_name' do
-      equip = build(:equipment, brand_name: 'Sokkia')
-      expect(equip.brand_name).to eq('Sokkia')
-    end
-  end
+  #   it 'has a brand_name' do
+  #     equip = build(:equipment, brand_name: 'Sokkia')
+  #     expect(equip.brand_name).to eq('Sokkia')
+  #   end
+  # end
 
-  describe 'supplier' do
-    it 'must have a supplier name' do
-      equip = build(:equipment, supplier: '')
-      expect(equip.supplier).to be_empty
-    end
+  # describe 'supplier' do
+  #   it 'must have a supplier name' do
+  #     equip = build(:equipment, supplier: '')
+  #     expect(equip.supplier).to be_empty
+  #   end
 
-    it 'has a supplier name' do
-      equip = build(:equipment, supplier: 'Skiddo Nig Ltd')
-      expect(equip.supplier).to eq('Skiddo Nig Ltd')
-    end
+  #   it 'has a supplier name' do
+  #     equip = build(:equipment, supplier: 'Skiddo Nig Ltd')
+  #     expect(equip.supplier).to eq('Skiddo Nig Ltd')
+  #   end
 
-    it 'has a supplier name with at least 3 characters long' do
-      equip = build(:equipment, supplier: 'Skiddo Nig Ltd')
-      expect(equip.supplier).to eq('Skiddo Nig Ltd')
-    end
-  end
+  #   it 'has a supplier name with at least 3 characters long' do
+  #     equip = build(:equipment, supplier: 'Skiddo Nig Ltd')
+  #     expect(equip.supplier).to eq('Skiddo Nig Ltd')
+  #   end
+  # end
 
-  describe 'serial_no' do
-    it 'must have a serial number' do
-      equip = build(:equipment, serial_no: '')
-      expect(equip.serial_no).to be_empty
-    end
+  # describe 'serial_no' do
+  #   it 'must have a serial number' do
+  #     equip = build(:equipment, serial_no: '')
+  #     expect(equip.serial_no).to be_empty
+  #   end
 
-    it 'must have a serial number' do
-      equip = build(:equipment, serial_no: 'we3435gyx78199023za')
-      expect(equip.serial_no).to eq('we3435gyx78199023za')
-    end
-  end
+  #   it 'must have a serial number' do
+  #     equip = build(:equipment, serial_no: 'we3435gyx78199023za')
+  #     expect(equip.serial_no).to eq('we3435gyx78199023za')
+  #   end
+  # end
 
-  describe 'description' do
-    it 'must have a description' do
-      equip = build(:equipment, serial_no: '')
-      expect(equip.serial_no).to be_empty
-    end
+  # describe 'description' do
+  #   it 'must have a description' do
+  #     equip = build(:equipment, serial_no: '')
+  #     expect(equip.serial_no).to be_empty
+  #   end
 
-    it 'has a description with at least 3 characters long' do
-      equip = build(:equipment, serial_no: 'we3435gyx78199023za')
-      expect(equip.serial_no).to eq('we3435gyx78199023za')
-    end
-  end
+  #   it 'has a description with at least 3 characters long' do
+  #     equip = build(:equipment, serial_no: 'we3435gyx78199023za')
+  #     expect(equip.serial_no).to eq('we3435gyx78199023za')
+  #   end
+  # end
 
   context 'validation tests' do
-    let(:equipment) { build(:equipment) }
-
-    it 'must have brand name specified' do
-      equipment.brand_name = ''
-      equipment.condition = 'broken'
-      equipment.model_number = 'we3435gyx7023za'
-      equipment.description = '3rd order Total station'
-      equipment.serial_no = '2344900292x87353'
-      equipment.supplier = 'Strafford & Sons'
-      equipment.date_acquired = '2022-09-21'
-      expect(equipment.save).to eq(false)
-    end
-
-    it 'has a brand name with at least 3 characters long' do
-      equipment.brand_name = 'tyro'
-      equipment.condition = 'broken'
-      equipment.model_number = 'we3435gyx7023za'
-      equipment.description = '3rd order Total station'
-      equipment.serial_no = '2344900292x87353'
-      equipment.supplier = 'Strafford & Sons'
-      equipment.date_acquired = '2022-09-21'
-      expect(equipment.save).to eq(true)
-    end
 
     describe 'brand name' do
       it 'is not stated' do
@@ -97,16 +74,6 @@ RSpec.describe Equipment, type: :model do
       end
     end
 
-    # it 'must have condition specified' do
-    #   equipment.brand_name = 'Sokkia'
-    #   equipment.condition = ''
-    #   equipment.model_number = 'we3435gyx7023za'
-    #   equipment.description = '3rd order Total station'
-    #   equipment.serial_no = '2344900292x87353'
-    #   equipment.supplier = 'Strafford & Sons'
-    #   equipment.date_acquired = '2022-09-21'
-    #   expect(equipment.save).to eq(false)
-    # end
 
     describe 'condition' do
       it 'is not stated' do
@@ -160,28 +127,6 @@ RSpec.describe Equipment, type: :model do
       end
     end
 
-    # it 'must have description' do
-    #   equipment.brand_name = 'Sokkia'
-    #   equipment.condition = 'broken'
-    #   equipment.model_number = 'we3435gyx7023za'
-    #   equipment.description = ''
-    #   equipment.serial_no = '2344900292x87353'
-    #   equipment.supplier = 'Strafford & Sons'
-    #   equipment.date_acquired = '2022-09-21'
-    #   expect(equipment.save).to eq(false)
-    # end
-
-    # it 'must have a serial number' do
-    #   equipment.brand_name = 'Sokkia'
-    #   equipment.condition = 'broken'
-    #   equipment.model_number = 'we3435gyx7023za'
-    #   equipment.description = '3rd order Total station'
-    #   equipment.serial_no = ''
-    #   equipment.supplier = 'Strafford & Sons'
-    #   equipment.date_acquired = '2022-09-21'
-    #   expect(equipment.save).to eq(false)
-    # end
-
     describe 'serial number' do
       it 'cannot be empty' do
         equipment = build(:equipment, brand_name: 'Sokkia', condition: 'broken', model_number: 'we3435gyx7023za', description: 'Ut cillum sit cillum cupidatat ullamco fugiat quis duis fugiat ullamco magna.', serial_no: '', supplier: 'Strafford & Sons', date_acquired: '2022-09-21')
@@ -199,16 +144,6 @@ RSpec.describe Equipment, type: :model do
       end
     end
 
-    # it 'must have a supplier' do
-    #   equipment.brand_name = 'Sokkia'
-    #   equipment.condition = 'intact'
-    #   equipment.model_number = 'we3435gyx7023za'
-    #   equipment.description = '3rd order Total station'
-    #   equipment.serial_no = '2344900292x87353'
-    #   equipment.supplier = ''
-    #   equipment.date_acquired = '2022-09-21'
-    #   expect(equipment.save).to eq(false)
-    # end
 
     describe 'supplier' do
       it 'cannot be empty' do
@@ -227,17 +162,6 @@ RSpec.describe Equipment, type: :model do
       end
     end
 
-    # it 'must have acquistion date' do
-    #   equipment.brand_name = 'Sokkia'
-    #   equipment.condition = 'intact'
-    #   equipment.model_number = 'we3435gyx7023za'
-    #   equipment.description = '3rd order Total station'
-    #   equipment.serial_no = '2344900292x87353'
-    #   equipment.supplier = 'Strafford & Sons'
-    #   equipment.date_acquired = ''
-    #   expect(equipment.save).to eq(false)
-    # end
-
     describe 'acquistion date' do
       it 'cannot be empty' do
         equipment = build(:equipment, brand_name: 'Sokkia', condition: 'broken', model_number: 'we3435gyx7023za', description: 'Ut cillum sit cillum cupidatat ullamco fugiat quis duis fugiat ullamco magna.', serial_no: '2344900292x87353', supplier: 'Strafford & Sons', date_acquired: '')
@@ -255,16 +179,6 @@ RSpec.describe Equipment, type: :model do
       # end
     end
 
-    # it 'must not be empty' do
-    #   equipment.brand_name = ''
-    #   equipment.condition = ''
-    #   equipment.model_number = ''
-    #   equipment.description = ''
-    #   equipment.serial_no = ''
-    #   equipment.supplier = ''
-    #   equipment.date_acquired = ''
-    #   expect(equipment.save).to eq(false)
-    # end
 
     describe 'All Fields' do
       it 'cannot be empty' do
