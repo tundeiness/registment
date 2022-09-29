14cc22bd5d7020cded255387e509c50b3b3091f2
tundeiness <tundeiness@yahoo.co.uk>
2022-09-29 17:08:39 +0100

    edit equipment factory

--- spec/factories/equipment.rb
+++ spec/factories/equipment.rb
@@ -1,25 +1,26 @@
 FactoryBot.define do
   factory :equipment do
-    # brand_name { Faker::Company.name }
-    # serial_no { Faker::Alphanumeric.alphanumeric(number: 15) }
+    brand_name { Faker::Company.name }
+    serial_no { Faker::Alphanumeric.alphanumeric(number: 15) }
     # condition { 'MyString' }
-    # trait :broken do
-    #   condition :broken
-    # end
+    trait :broken do
+      condition { 'broken' }
+    end
 
-    # trait :intact do
-    #   condition :intact
-    # end
-    # date_acquired { Faker::Date.in_date_period }
-    # supplier { Faker::Name.name }
-    # model_number { Faker::Company.duns_number }
-    # description { Faker::Lorem.paragraph }
-    brand_name { 'MyString' }
-    serial_no { 'MyString' }
-    date_acquired { '2022-09-21' }
-    supplier { 'MyString' }
-    model_number { 'MyString' }
-    description { 'MyText' }
+    trait :intact do
+      condition { 'intact' }
+    end
+    date_acquired { Faker::Date.in_date_period }
+    supplier { Faker::Name.name }
+    model_number { Faker::Company.duns_number }
+    description { Faker::Lorem.paragraph }
+    # brand_name { 'MyString' }
+    # serial_no { 'MyString' }
+    # date_acquired { '2022-09-21' }
+    # supplier { 'MyString' }
+    # model_number { 'MyString' }
+    # description { 'MyText' }
+    # email { "#{brand_name}.#{serial_no}@example.com".downcase }
   end
 end
 # equip = FactoryBot.create(:equipment)
