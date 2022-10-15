64e42e5ed93b9080d1ee41ec6082c5e9296f89a0
tundeiness <tundeiness@yahoo.co.uk>
2022-10-15 13:14:42 +0100

    add validation for service date

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -7,6 +7,7 @@ class Equipment < ApplicationRecord
   end
   validates :brand_name, :serial_no, :condition, :date_acquired, :supplier, :model_number, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { in: 10..200 }
+  validates :service_date
 
   scope :newest, -> { order(created_at: :asc) }
   # scope :oldest, -> { order(created_at: :desc) }
