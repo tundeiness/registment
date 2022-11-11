4734eb48f68e3c1999c3907e827a9cb70913e8f6
tundeiness <tundeiness@yahoo.co.uk>
2022-11-11 17:08:34 +0100

    create bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_162014) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_160228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_162014) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "equipment_id", null: false
+    t.string "status"
+    t.date "booking_date"
+    t.text "description"
+    t.date "pickup_date"
+    t.date "return_date"
+    t.decimal "booking_price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "equipment", force: :cascade do |t|
@@ -77,4 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_162014) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "equipment"
+  add_foreign_key "bookings", "users"
 end
