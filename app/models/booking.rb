2e71050430483c4875eb895e5496e196a5181953
tundeiness <tundeiness@yahoo.co.uk>
2022-11-15 17:21:48 +0100

    create set_status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :equipment
+
+  enum status: %i[available reserved checkoedOut returned]
+  after_initialize :set_status, if: :new_record
+
+  def set_status
+    self.status ||= :available if status.nil?
+  end
 end
