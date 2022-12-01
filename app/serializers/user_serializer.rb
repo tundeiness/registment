ab188b7e3599c41a761fac0cf72c993f12a92b6d
tundeiness <tundeiness@yahoo.co.uk>
2022-12-01 15:51:20 +0100

    edit user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :first_name, :last_name, :email, :password, :address, :phone
+  has_many :booking
+  has_many :equipment
 end
