f48a323d98957703600f422bf8893051a309c245
tundeiness <tundeiness@yahoo.co.uk>
2022-12-01 15:43:26 +0100

    edit applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
-  before_filter :configure_permitted_parameters
+  before_action :configure_permitted_parameters
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |param| param.permit(:first_name, :last_name, :email, :phone, :address, :password, :current_password) }
   end
 end
