d5b35e31bab76d238f9f0cbb1527c0a64f9e6c09
tundeiness <tundeiness@yahoo.co.uk>
2022-12-12 16:56:41 +0100

    create Api controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::EquipmentsController < ApplicationController
+  load_and_authorize_resource
+  # before_action :set_company, only: [:show, :update, :destroy]
 
   def index
     @equipments ||= Equipment.all
@@ -14,13 +16,23 @@ class Api::V1::EquipmentsController < ApplicationController
   def show
     @equipments = Equipment.find(params[:id])
     if @equipments.photo.attached? 
-      render json: @equipments.as_json(only: %i[brand_name]).merge(photo_path: url_for(@equipments.avatar))
-    else 
-      render json: @equipments.as_json only: :brand_name # rubocop:disable Lint/Syntax
-    end 
-  end 
+      render json: @equipments.as_json(only: %i[brand_name]).merge(photo_path: url_for(@equipments.avatar)), status: :ok
+    else
+      render json: @equipments.as_json only: :brand_name, status: :ok # rubocop:disable Lint/Syntax
+    end
+  end
 
-  private 
+  def create
+    @equipment = equipment.new(equipment_params)
+    if @equipment.save
+      render json: @equipment, status: :ok
+    else
+      render json: { data: @equipment.errors.full_messages, status: "failed" },
+        status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def equipment_params
     params.require(:equipment).permit(:brand_name, :photo, :serial_no, :condition, :date_acquired, :supplied_by, :model_number, :description)
