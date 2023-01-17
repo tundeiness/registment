fa165816bca4d08bb2bf5374b204e4d1e1362fd0
tundeiness <tundeiness@yahoo.co.uk>
2023-01-17 17:06:26 +0100

    clean up unwanted code blocks

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,18 +1,10 @@
 class Equipment < ApplicationRecord
   # around_save :update_service, :update_service_date
   before_save :update_service
-  # before_create :update_service
-
-  # SERVICE_DAYS = Date.today.advance(days: 30)
   SERVICE_DAYS = 30.days
-  # after_commit :update_service, :update_service_date
 
-  # belongs_to :user
   has_one_attached :featured_image
-  # has_one_attached :photo do |photo|
-  #   photo.variant :thumbnail, resize_to_limit: [100, 100]
-  #   photo.variant :medium, resize_to_limit: [400, 400]
-  # end
+
   validates :brand_name, :serial_no, :condition, :date_acquired, :supplier, :model_number, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { in: 10..200 }
   # validates :service_date
@@ -24,7 +16,9 @@ class Equipment < ApplicationRecord
   has_many :users, through: :bookings
 
   scope :newest, -> { order(created_at: :asc) }
-  # scope :oldest, -> { order(created_at: :desc) }
+
+  private
+
   def update_service
     self.service_date = if service_date.blank?
                           (date_acquired).to_date + SERVICE_DAYS
@@ -32,15 +26,4 @@ class Equipment < ApplicationRecord
                           (service_date).to_date + SERVICE_DAYS
                         end
   end
-
-  # def photo_url
-  #   url_for(photo)
-  # end
-
-
-  # def update_service_date
-  #   ActiveRecord::Base.transaction do
-  #     user.update_column(created_at: :date_acquired)
-  #   end
-  # end
 end
