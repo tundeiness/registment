c94c9e8f251639b790ede4a94ae13739388e2450
tundeiness <tundeiness@yahoo.co.uk>
2023-01-17 16:53:33 +0100

    remove excess code from equipments controller

--- app/controllers/api/v1/equipments_controller.rb
+++ app/controllers/api/v1/equipments_controller.rb
@@ -1,18 +1,10 @@
 class Api::V1::EquipmentsController < ApplicationController
   load_and_authorize_resource
   before_action :set_equipment, only: [:show, :destroy]
-  # before_action :set_company, only: [:show, :update, :destroy]
 
   def index
     @equipments = Equipment.all
     render(json: @equipments)
-    # render json: @equipments.map { |item|
-    #   if item.photo.attached?
-    #     item.as_json(only: :brand_name).merge(photo_path: url_for(item.photo))
-    #   else
-    #     item.as_json only: :brand_name
-    #   end
-    # }
   end
 
   def show
@@ -20,20 +12,16 @@ class Api::V1::EquipmentsController < ApplicationController
     if @equipment.featured_image.attached? 
       render json: @equipment.as_json(only: %i[brand_name]).merge(featured_image_path: url_for(@equipment.avatar)), status: :ok
     else
-      # render json: @equipments.as_json only: :brand_name # rubocop:disable Lint/Syntax
       render json: @equipment, status: :ok
     end
   end
 
   def create
-    # @user = current_user
     @equipment = Equipment.new(equipment_params)
-    # @equipment = current_user.equipment.new(equipment_params)
     if @equipment.save
       render json: @equipment, status: :created
     else
       render json: { errors: @equipment.errors.full_messages, status: 'failed' }, status: :unprocessable_entity
-      # render json: { errors: post.errors }, status: 400
     end
   end
 
@@ -57,13 +45,6 @@ class Api::V1::EquipmentsController < ApplicationController
 
 
   private
-
-  # def respond_to_equipment()
-  #   if @equipment.valid?
-  #     equipment_serializer = EquipmentSerializer.new(equipment: @equipment, user: @user.current_user)
-  #     render json equipment_serializer.
-  #   end
-  # end
 
   def set_equipment
     # @equipment = cureent_user.equipment.find(params[:id])
