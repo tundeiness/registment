dfa8df906b24bfa2eb8c570fb3ebcd429093044d
tundeiness <tundeiness@yahoo.co.uk>
2023-01-17 17:10:02 +0100

    remove excess code

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,24 +14,14 @@ class User < ApplicationRecord
   has_many :equipment, through: :bookings
 
   # has_many :equipments
-  # accepts_nested_attributes_for :bookings, allow_destroy: true
 
   def jwt_payload
     super
   end
-
-  # before_create :set_user_role
-
-  # has_many :bookings, class_name: 'Bookings', foreign_key: 'customer_id'
-  # belongs_to :user, class_name: 'User', foreign_key: 'customer_id'
 
   ROLES.each do |name|
     define_method "#{name}?" do
       role == name
     end
   end
-
-  # def set_user_role
-  #   self.role = 'user'
-  # end
 end
