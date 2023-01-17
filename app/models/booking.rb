919e65701ac917a12f1e208f1045894cc8584f5a
tundeiness <tundeiness@yahoo.co.uk>
2023-01-17 16:54:33 +0100

    remove excess code from bookings.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,14 +6,7 @@ class Booking < ApplicationRecord
   validates :status, :booking_date, :description, :pickup_date, :return_date, :booking_price, presence: true
 
   # enum status: %i[available reserved checkedOut returned]
-  # after_initialize :set_status, if: :new_record
 
-  # def set_status
-    # self.status ||= :available if status.nil?
-    # self.set_status ||= :available if status.nil?
-  # end
-
-  # delegate :service_date, to: :equipment
   private
 
   def update_equipment_status
