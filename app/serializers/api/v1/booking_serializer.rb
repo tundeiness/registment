5316502260212a8c2a8e92915cb373f1f632e3b2
tundeiness <tundeiness@yahoo.co.uk>
2023-01-17 13:32:25 +0100

    add attributes to booking serializer

--- app/serializers/api/v1/booking_serializer.rb
+++ app/serializers/api/v1/booking_serializer.rb
@@ -1,3 +1,3 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :status, :booking_date, :description, :pickup_date, :return_date, :booking_price, :user_id, :equipment_id
 end
