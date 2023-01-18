d5a95dbf78bdc95b59783c6b26963b89172de722
tundeiness <tundeiness@yahoo.co.uk>
2023-01-18 14:22:24 +0100

    add JWT helper to rails helper.rb file
    add user spec
    
    setup user factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
-# FactoryBot.define do
-#   factory :user do
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.cell_phone_with_country_code }
+    address { Faker::Address.full_address }
+    password { '123456' }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a user' do
+    let(:user) { build :user }
+    it 'should be valid user with all attributes' do
+      user.valid? == true
+    end
+  end
 end
