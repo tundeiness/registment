afe28872480952b60a5353b3d3fa93d5d557bbb8
tundeiness <tundeiness@yahoo.co.uk>
2023-02-07 15:50:16 +0100

    edit devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -323,7 +323,7 @@ Devise.setup do |config|
       ['POST', %r{^/users/sign_in$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/users/sign_out}]
+      ['DELETE', %r{^/users/sign_out$}]
     ]
     jwt.expiration_time = 120.minutes.to_i
   end
