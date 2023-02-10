33496da6b94d708429fd117ca9332a9d4751b036
tundeiness <tundeiness@yahoo.co.uk>
2023-02-10 14:37:43 +0100

    fix bug in test

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -7,7 +7,7 @@ class Equipment < ApplicationRecord
 
   validates :brand_name, :serial_no, :condition, :date_acquired, :supplier, :model_number, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { in: 10..200 }
-  # validates :service_date
+  validates :service_date, presence: true
   validates :loan_status, presence: true
 
   # validates :featured_image, { presence: true }
