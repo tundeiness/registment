5d0e9188a8b865194748386193cb094943d78fab
tundeiness <tundeiness@yahoo.co.uk>
2023-02-13 17:10:57 +0100

    complete model test

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:booking_date) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:pickup_date) }
+    it { should validate_presence_of(:return_date) }
+    it { should validate_presence_of(:booking_price) }
+  end
+
+  context 'Model Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:equipment) }
+    it { should_not have_many(:user) }
+    it { should_not have_many(:equipment) }
+    it { should_not belong_to(:invalid_association) }
+    it { should_not have_many(:invalid_association) }
+  end
 end
