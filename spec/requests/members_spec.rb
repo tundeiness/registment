a95ca08f4e6716acdbfe3d313ac73657470bbc3c
tundeiness <tundeiness@yahoo.co.uk>
2023-03-06 11:41:36 +0100

    add test for normal user

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Members', type: :request do
   describe 'GET /members' do
     let(:user) { create(:user, :super_admin) }
+    let(:normal_user) { create(:user, :normal) }
     let(:params) do
       {
         user: {
@@ -12,14 +13,38 @@ RSpec.describe 'Members', type: :request do
       }
     end
 
-    before do
-      post '/users/sign_in', params: params
+    let(:params_two) do
+      {
+        normal_user: {
+          email: user.email,
+          password: user.password
+        }
+      }
     end
 
-    it 'returns http success' do
-      get '/members'
-      expect(response).to have_http_status(200)
-      expect(response).to have_http_status(:success)
+    context 'User with a super_admin role' do
+      before do
+        post '/users/sign_in', params: params
+      end
+
+      it 'can get all registered users' do
+        get '/members'
+
+        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'User with a normal role' do
+      before do
+        post '/users/sign_in', params: params_two
+      end
+
+      it 'cannot get all registered users' do
+        get '/members'
+
+        expect(response).to have_http_status(401)
+      end
     end
   end
 end
