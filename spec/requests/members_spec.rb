8783962b3f7de9c09a01323b00c7b39c04997d73
tundeiness <tundeiness@yahoo.co.uk>
2023-03-06 10:04:54 +0100

    add members spec

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Members", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/members/index"
+RSpec.describe 'Members', type: :request do
+  describe 'GET /members' do
+    it 'returns http success' do
+      get '/members'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
