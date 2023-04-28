3deaa7fcf1dccbd9ae5267efa364a07fdbb67d40
tundeiness <tundeiness@yahoo.co.uk>
2023-04-28 12:16:43 +0100

    add custom method

--- app/controllers/api/v1/equipment_conditions_controller.rb
+++ app/controllers/api/v1/equipment_conditions_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::EquipmentConditionsController < ApplicationController
     equipment_conditions = EquipmentCondition.all
     render json: { equipment_conditions: equipment_conditions }
   end
+
+  def count_by_condition
+    condition_count = EquipmentCondition.find_by(condition: params[:condition]).count
+    render json: { count: condition_count }
+  end
 end
