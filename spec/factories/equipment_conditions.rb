04869a07f2ab08e35179fd282a5b2b151780255e
tundeiness <tundeiness@yahoo.co.uk>
2023-04-28 13:32:41 +0100

    implement factory for equipment condition

--- spec/factories/equipment_conditions.rb
+++ spec/factories/equipment_conditions.rb
@@ -1,6 +1,24 @@
 FactoryBot.define do
   factory :equipment_condition do
-    condition { "MyString" }
-    count { 1 }
+    equipment
+    condition { %w[new intact need_repair damaged].sample }
+
+    trait :new do
+      condition { :new }
+    end
+
+    trait :need_repair do
+      condition { :need_repair }
+    end
+
+    trait :damaged do
+      condition { :damaged }
+    end
+
+    trait :intact do
+      condition { :intact }
+    end
+
+    count { Faker::Number.non_zero_digit }
   end
 end
