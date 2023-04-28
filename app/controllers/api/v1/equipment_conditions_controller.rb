a30b39e5964b73c0f83ebc2dbd78a2eef8fe2f59
tundeiness <tundeiness@yahoo.co.uk>
2023-04-28 11:38:51 +0100

    create index action for equipment conditions

--- app/controllers/api/v1/equipment_conditions_controller.rb
+++ app/controllers/api/v1/equipment_conditions_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::EquipmentConditionsController < ApplicationController
+  def index
+    equipment_conditions = EquipmentCondition.all
+    render json: { equipment_conditions: equipment_conditions }
+  end
 end
