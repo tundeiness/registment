56225b808e735eee3763420841a81ab251ac2511
tundeiness <tundeiness@yahoo.co.uk>
2023-05-02 18:34:30 +0100

    test equipment condition

--- spec/models/equipment_condition_spec.rb
+++ spec/models/equipment_condition_spec.rb
@@ -2,14 +2,43 @@ require 'rails_helper'
 
 RSpec.describe EquipmentCondition, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  context 'validations' do
+  describe 'equipment condition' do
     subject { build(:equipment_condition) }
 
-    it { should validate_presence_of(:count) }
-    it { should validate_presence_of(:condition) }
-  end
+    context 'validation' do
+      it { should validate_presence_of(:count) }
+      it { should validate_presence_of(:condition) }
+    end
 
-  context 'model associations' do
-    it { should belong_to(:equipment) }
+    context 'model associations' do
+      it { should belong_to(:equipment) }
+    end
+  end
+  
+
+  describe 'after_commit :update_condition_count' do
+    let(:equipment) { create(:equipment, condition: "new") }
+
+    context 'when equipment condition is updated' do
+      before do
+        equipment.update(condition: 'Need Repair')
+      end
+
+      it "should update the corresponding EquipmentCondition record" do
+        expect(EquipmentCondition.find_by(condition: "New").count).to eq(0)
+        expect(EquipmentCondition.find_by(condition: "Need Repair").count).to eq(1)
+      end
+    end
+
+    # context "when equipment condition is not updated" do
+    #   before do
+    #     equipment.update(name: "Test Equipment")
+    #   end
+
+    #   it "should not update any EquipmentConditionCount record" do
+    #     expect(EquipmentConditionCount.find_by(condition: "New").count).to eq(0)
+    #     expect(EquipmentConditionCount.find_by(condition: "Need Repair").count).to eq(0)
+    #   end
+    # end
   end
 end
