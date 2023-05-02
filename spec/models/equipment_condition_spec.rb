61d6e10bb598557e698be6a376b5b5961f4c131a
tundeiness <tundeiness@yahoo.co.uk>
2023-05-02 18:01:45 +0100

    add model test for eequipment and equipmnet_conditions

--- spec/models/equipment_condition_spec.rb
+++ spec/models/equipment_condition_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe EquipmentCondition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { build(:equipment_condition) }
+
+    it { should validate_presence_of(:count) }
+    it { should validate_presence_of(:condition) }
+  end
+
+  context 'model associations' do
+    it { should belong_to(:equipment) }
+  end
 end
