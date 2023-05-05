3608b4841c50585c2ed45e28c1d32a073e6b7a00
tundeiness <tundeiness@yahoo.co.uk>
2023-05-05 15:30:15 +0100

    edit equipment condition

--- app/models/equipment_condition.rb
+++ app/models/equipment_condition.rb
@@ -1,6 +1,6 @@
 class EquipmentCondition < ApplicationRecord
-  belongs_to :equipment
+  # belongs_to :equipment
 
-  validates :num, presence: true
-  validates :condition, presence: true
+  # validates :num, presence: true
+  # validates :condition, presence: true
 end
