e48a108ef6dd6ed1217340e822c1a2f8bfd7c41b
tundeiness <tundeiness@yahoo.co.uk>
2023-05-12 17:56:28 +0100

    rspec test for update usage count

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe Booking, type: :model do
     it { should_not belong_to(:invalid_association) }
     it { should_not have_many(:invalid_association) }
   end
+
+
+  context 'method' do
+    describe '#update_usage_count' do
+      let(:equipment) { create(:equipment) }
+      let(:booking) { create(:booking, equipment: equipment) }
+
+      it 'should update the usage count when the status is changed to loaned out' do
+        expect {
+          booking.update(status: 'loaned_out')
+        }.to change { equipment.usage_count }.by(1)
+      end
+
+      it 'should not update the usage count when the status is not changed to loaned out' do
+        expect {
+          booking.update(status: 'Available')
+        }.not_to change { equipment.usage_count }
+      end
+    end
+  end
 end
