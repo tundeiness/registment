211e3ed4dc331b94d3da6c567c1f1d3ec7968473
tundeiness <tundeiness@yahoo.co.uk>
2023-05-12 17:49:40 +0100

    add update usage count

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :equipment
+  before_save :update_usage_count
   after_save :update_equipment_status
 
   validates :status, :booking_date, :description, :pickup_date, :return_date, :booking_price, presence: true
@@ -11,5 +12,9 @@ class Booking < ApplicationRecord
 
   def update_equipment_status
     equipment.update(loan_status: status)
+  end
+
+  def update_usage_count
+    equipment.update(usage_count: equipment.usage_count += 1) if status_changed? && status == 'loaned_out'
   end
 end
