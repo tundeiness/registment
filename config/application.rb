5b8833a2a9fc3c7e6ec4b61f49cc77e123edc391
tundeiness <tundeiness@yahoo.co.uk>
2024-03-27 16:35:22 -0400

    run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module Registment
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -31,18 +36,9 @@ module Registment
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of  middleware suitable for API only apps.
+    # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use config.session_store, config.session_options
-
-    config.generators.system_tests = nil
-    config.generators do |g|
-      g.test_framework(:rspec, fixtures: false, view_specs: false)
-      # g.test_framework(:rspec, fixtures: false, view_specs: false, routing_specs: false)
-    end
   end
 end
